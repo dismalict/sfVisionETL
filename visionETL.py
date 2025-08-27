b508fea857b66d7ec2e7906827409af87956ecef
Mason <dismal.ict@outlook.com>
2025-08-27 08:22:34 -0500

    Update visionETL.py

--- visionETL.py
+++ visionETL.py
@@ -103,6 +103,40 @@ def run_etl(config):
                 orin_row["cudnn"], orin_row["tensorrt"], orin_row["vpi"],
                 orin_row["vulkan"], orin_row["opencv"]
             ))
+        print("DEBUG: Insert column count:",  len([
+    "hostname", "pool_timestamp", "workstation", "status", "max_people", "time_work",
+    "orin_id", "orin_time", "uptime", "CPU1", "CPU2", "CPU3", "CPU4", "CPU5", "CPU6",
+    "RAM", "SWAP", "EMC", "GPU", "APE", "NVDEC", "NVJPG", "NVJPG1", "OFA", "SE", "VIC",
+    "Fan_pwmfan0", "Temp_CPU", "Temp_CV0", "Temp_CV1", "Temp_CV2", "Temp_GPU", "Temp_SOC0",
+    "Temp_SOC1", "Temp_SOC2", "Temp_tj", "Power_CPU", "Power_CV", "Power_GPU", "Power_SOC",
+    "Power_SYS5v", "Power_VDDRQ", "Power_tj", "Power_TOT", "jetson_clocks", "nvp_model",
+    "disk_available_gb", "ip_address", "model", "jetpack", "l4t", "nv_power_mode",
+    "serial_number", "p_number", "module", "distribution", "release", "cuda", "cudnn",
+    "tensorrt", "vpi", "vulkan", "opencv"
+        ]))
+        print("DEBUG: Values count:", len((
+            host,
+            pool_row["Timestamp"], pool_row["WorkStation"], pool_row["Status"],
+            pool_row["Max_People"], pool_row["Time_Work"],
+            orin_row["id"], orin_row["time"], orin_row["uptime"],
+            orin_row["CPU1"], orin_row["CPU2"], orin_row["CPU3"], orin_row["CPU4"],
+            orin_row["CPU5"], orin_row["CPU6"], orin_row["RAM"], orin_row["SWAP"],
+            orin_row["EMC"], orin_row["GPU"], orin_row["APE"], orin_row["NVDEC"],
+            orin_row["NVJPG"], orin_row["NVJPG1"], orin_row["OFA"], orin_row["SE"],
+            orin_row["VIC"], orin_row["Fan pwmfan0"], orin_row["Temp CPU"],
+            orin_row["Temp CV0"], orin_row["Temp CV1"], orin_row["Temp CV2"],
+            orin_row["Temp GPU"], orin_row["Temp SOC0"], orin_row["Temp SOC1"],
+            orin_row["Temp SOC2"], orin_row["Temp tj"], orin_row["Power CPU"],
+            orin_row["Power CV"], orin_row["Power GPU"], orin_row["Power SOC"],
+            orin_row["Power SYS5v"], orin_row["Power VDDRQ"], orin_row["Power tj"],
+            orin_row["Power TOT"], orin_row["jetson_clocks"], orin_row["nvp model"],
+            orin_row["disk_available_gb"], orin_row["ip_address"], orin_row["model"],
+            orin_row["jetpack"], orin_row["l4t"], orin_row["nv_power_mode"],
+            orin_row["serial_number"], orin_row["p_number"], orin_row["module"],
+            orin_row["distribution"], orin_row["release"], orin_row["cuda"],
+            orin_row["cudnn"], orin_row["tensorrt"], orin_row["vpi"],
+            orin_row["vulkan"], orin_row["opencv"]
+        )))
 
         # prune older than 7 days
         dst_cursor.execute("""
